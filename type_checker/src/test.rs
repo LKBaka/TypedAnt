733fe8abd3a303138270a922b8c8a6768e46b0b5
APIBox <LinkerVB@outlook.com>
2025-11-14 13:46:16 +0800

    新增对于 TypeChecker 类型化 Ident 类型化 LetStatement 的测试

--- type_checker/src/test.rs
+++ type_checker/src/test.rs
@@ -1,2 +1,101 @@
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use std::rc::Rc;
+
+    use bigdecimal::BigDecimal;
+    use token::{token::Token, token_type::TokenType};
+
+    use crate::{
+        Ty, TypeChecker,
+        table::TypeTable,
+        typed_ast::{GetType, typed_expr::TypedExpression, typed_stmt::TypedStatement},
+    };
+
+    #[test]
+    fn test_checker_var_get() {
+        let file: Rc<str> = "__test_checker_var_get__".into();
+
+        let table = &mut TypeTable::new();
+
+        table.define_var("a", crate::Ty::BigInt);
+
+        let checker = &mut TypeChecker { table };
+
+        let ident_raw = ast::expressions::ident::Ident {
+            token: Token::new("a".into(), TokenType::Ident, file.clone(), 1, 1),
+            value: "a".into(),
+        };
+
+        let result = checker
+            .check_expr(ast::expr::Expression::Ident(ident_raw))
+            .unwrap();
+
+        assert!(matches!(result, TypedExpression::Ident(..)));
+
+        let (ident, ty) = match result {
+            TypedExpression::Ident(it, ty) => (it, ty),
+            _ => unreachable!(),
+        };
+
+        assert!(ident.value == "a".into());
+        assert!(ty == Ty::BigInt);
+
+        println!("ok! ident.value: {}, ty: {ty:#?}", ident.value)
+    }
+
+    #[test]
+    fn test_checker_var_def() {
+        let file: Rc<str> = "__test_checker_var_def__".into();
+
+        let table = &mut TypeTable::new();
+
+        let checker = &mut TypeChecker { table };
+
+        let let_stmt_raw = ast::stmt::Statement::Let {
+            name: ast::expressions::ident::Ident {
+                token: Token::new("a".into(), TokenType::Ident, file.clone(), 1, 5),
+                value: "a".into(),
+            },
+            value: ast::expr::Expression::BigInt {
+                token: Token::new("1".into(), TokenType::IntegerBig, file.clone(), 1, 17),
+                value: BigDecimal::from(1),
+            },
+            var_type: Some(ast::expressions::ident::Ident {
+                token: Token::new("BigInt".into(), TokenType::IntegerBig, file.clone(), 1, 8),
+                value: "BigInt".into(),
+            }),
+            token: Token::new("let".into(), TokenType::Let, file.clone(), 1, 1),
+        };
+
+        let result = checker.check_statement(let_stmt_raw).unwrap();
+
+        assert!(matches!(result, TypedStatement::Let { .. }));
+
+        let (ident, ty) = match result {
+            TypedStatement::Let { name, ty, .. } => (name, ty),
+            _ => unreachable!(),
+        };
+
+        assert!(ident.value == "a".into());
+        assert!(ty == Ty::BigInt);
+
+        let get_symbol_result = table.get("a");
+
+        assert!(get_symbol_result.is_some());
+        assert!(unsafe { get_symbol_result.unwrap_unchecked() }.name == "a".into());
+        assert!(
+            unsafe { get_symbol_result.unwrap_unchecked() }
+                .ty
+                .get_type()
+                == Ty::BigInt
+        );
+
+        println!(
+            "ok! symbol name: {}, symbol ty: {:#?}",
+            unsafe { get_symbol_result.unwrap_unchecked() }.name,
+            unsafe { get_symbol_result.unwrap_unchecked() }
+                .ty
+                .get_type()
+        )
+    }
+}
